ec07f8cbecc53b938cd4fd8a877c0e3c085e9d59
seoktaehyeon <seoktaehyeon@msn.com>
2020-09-09 13:30:37 +0800

    change db to Pg since Sonar 7.9 is not support MySQL

--- migrate_docker_images.sh
+++ migrate_docker_images.sh
@@ -13,8 +13,12 @@ source_img="dinkel/phpldapadmin:latest"
 target_img="${registry}/${repo}/phpldapadmin:latest"
 docker pull ${source_img} && docker tag ${source_img} ${target_img} && docker push ${target_img}
 
-source_img="mysql:5.7"
-target_img="${registry}/${repo}/mysql:5.7"
+#source_img="mysql:5.7"
+#target_img="${registry}/${repo}/mysql:5.7"
+#docker pull ${source_img} && docker tag ${source_img} ${target_img} && docker push ${target_img}
+
+source_img="postgres:10"
+target_img="${registry}/${repo}/postgres:10"
 docker pull ${source_img} && docker tag ${source_img} ${target_img} && docker push ${target_img}
 
 source_img="gitlab/gitlab-ce:13.3.5-ce.0"
